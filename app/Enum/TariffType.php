1c9a29cd434a5c9ae70ce7e234fbdfcce2366da2
Věra Hellebrand <vera.hellebrand@fapi.cz>
2025-06-28 13:03:58 +0200

    Added Enum folder and TariffType enum

---
+++ app/Enum/TariffType.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace App\Enum;
+
+enum TariffType: string
+{
+
+	case NEO_MODRY = 'neo_modry';
+	case NEO_STRIBRNY = 'neo_stribrny';
+	case NEO_PLATINOVY = 'neo_platinovy';
+	case MUJ_PRVNI_TARIF = 'muj_prvni_tarif';
+
+	public function label(): string
+	{
+		return match($this) {
+			self::NEO_MODRY => 'NEO Modrý',
+			self::NEO_STRIBRNY => 'NEO Stříbrný',
+			self::NEO_PLATINOVY => 'NEO Platinový',
+			self::MUJ_PRVNI_TARIF => 'Můj první tarif',
+		};
+	}
+
+}
