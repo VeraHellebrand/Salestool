603a2b297fcffdc89cb92faf8055842659d4463d
Věra Hellebrand <vera.hellebrand@fapi.cz>
2025-06-28 11:32:32 +0200

    Added migration script for running SQL migrations

---
+++ scripts/migrate.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+require __DIR__ . '/../vendor/autoload.php';
+
+use Dibi\Connection;
+
+$db = new Connection([
+	'driver' => 'sqlite3',
+	'database' => __DIR__ . '/../database.sqlite',
+]);
+
+$migrationDir = __DIR__ . '/../migrations';
+$files = glob($migrationDir . '/*.sql');
+sort($files);
+
+foreach ($files as $file) {
+	$sql = file_get_contents($file);
+	$db->nativeQuery($sql);
+	echo "Migrace $file byla úspěšně provedena.\n";
+}
+
+echo "Všechny migrace proběhly.\n";
