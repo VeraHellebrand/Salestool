998152175de83776972a8950b75946b778390569
Věra Hellebrand <vera.hellebrand@fapi.cz>
2025-06-28 11:07:36 +0200

    Created initial project structure and tariffs migration

---
+++ config/bootstrap.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types = 1);
+
+use Nette\Bootstrap\Configurator;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$configurator = new Configurator();
+$configurator->setTempDirectory(__DIR__ . '/../temp');
+$configurator->addConfig(__DIR__ . '/common.neon');
+
+return $configurator->createContainer();
